05be0f323f3d5a4cfd65c32532267f1dc0632a4e
Beatrice <pedretti.btrc@gmail.com>
2021-06-24 17:24:12 +0200

    matlab script

--- interpolation_plane.m
+++ interpolation_plane.m
@@ -5,6 +5,9 @@
 coord = importdata ('coordinates_plane_2.txt')
 
 %% POINT CLOUD 
+%DISCLAIMER: point cloud functions need to have computer vision toolbox
+%installed in your environment. Further ahead you can find the code to
+%compute a regression plane without the need for computer vision toolbox
 %create and display point cloud
 ptCloud = pointCloud(coord);
 figure
@@ -14,20 +17,28 @@ ylabel('Y(m)')
 zlabel('Z(m)')
 title('Original Point Cloud')
 
+%native MATLAB function to fit a regression plane and compute MSE (mean
+%square error)
 maxDistance = 0.1;
 [model,inlierIndices,outlierIndices,meanError] = pcfitplane(ptCloud,maxDistance) 
 
 %% REGRESSION PLANE
+%another method to compute regression plane that does NOT need Computer
+%Vision Toolbox
+
+%save x y z vectors
 x = coord(:,1)
 y = coord(:,2)
 z = coord(:,3)
 
+%plot x y z points
 figure(1)
 stem3(x, y, z)
 grid on
 xv = linspace(min(x), max(x), 20);
 yv = linspace(min(y), max(y), 20);
 [X,Y] = meshgrid(xv, yv);
+%regression plane equation
 B = [x(:) y(:) ones(size(x(:)))] \ z(:);
 Z = X*B(1) + Y*B(2) + B(3);
 hold on
