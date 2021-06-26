fe3e155303d911100a83d5b378240a47be6635da
Beatrice <pedretti.btrc@gmail.com>
2021-06-26 15:28:28 +0200

    fixing check on distance

--- FinalProject_1-1.cydsn/sonar.c
+++ FinalProject_1-1.cydsn/sonar.c
@@ -30,20 +30,24 @@ void find_position ()
     pos_servo2_rad=(double)(Servo_GetPosition2()+ANGLE_ZERO-SERVO_MID_ANGLE)*(PI / SERVO_LIMIT_H);
     pos_servo2_z_rad=(double)(Servo_GetPosition2()+ANGLE_ZERO)*(PI / SERVO_LIMIT_H);
 
-    //computation of coordinates
+    //setting distance_real = distance because distance is computed in ISR and may change during computation of coordinates
+    // by setting distance_real = distance and using only distance_real we keep one value of distance for the computation
     distance_real = distance;
     
     //if the point is within a certain distance
     if(distance_real<DISTANCE_TH_H && distance_real>DISTANCE_TH_L)
-    {     
-        y=(D2+(ARM_LENGTH+distance_real)*cos(pos_servo2_rad))*cos(pos_servo1_rad);
-        x=(D2+(ARM_LENGTH+distance_real)*cos(pos_servo2_rad))*sin(pos_servo1_rad);
+    {   
         z=Z1-(ARM_LENGTH+distance_real)*cos(pos_servo2_z_rad);
-        //send coordinates to UART
-        sprintf (message, "%d %d %d\r\n", (int16_t)x,(int16_t)y,(int16_t)z);
-        UART_1_PutString(message);
-        //if at least one point is found in the row, set this flag to 1
-        flag_print = 1;  
+        if (z >= 0)
+        {
+            y=(D2+(ARM_LENGTH+distance_real)*cos(pos_servo2_rad))*cos(pos_servo1_rad);
+            x=(D2+(ARM_LENGTH+distance_real)*cos(pos_servo2_rad))*sin(pos_servo1_rad);            
+            //send coordinates to UART
+            sprintf (message, "%d %d %d\r\n", (int16_t)x,(int16_t)y,(int16_t)z);
+            UART_1_PutString(message);
+            //if at least one point is found in the row, set this flag to 1
+            flag_print = 1;  
+        }
     }
 }
 /* [] END OF FILE */
